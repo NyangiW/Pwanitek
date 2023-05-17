1312f3b70441fab7efc44464c147b8e6db8cadf4
NyangiW <anyangowinnie9@gmail.com>
2023-05-17 21:43:15 +0300

    Backend Changes

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -13,86 +13,101 @@ class Landing extends StatelessWidget {
     return Scaffold(
       //body
       body: Container(
-         padding: const EdgeInsets.all(20),
-          width: Get.width,
-          height: Get.height,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("images/dash.jpg"), fit: BoxFit.cover),
-          ),
-          //text
-          // ignore: prefer_const_literals_to_create_immutables
-          child: Column(children: [Center(
-            child: const Text("Welcome to Kutoa!",
+        padding: const EdgeInsets.all(20),
+        width: Get.width,
+        height: Get.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/dash.jpg"), fit: BoxFit.cover),
+        ),
+        //text
+        // ignore: prefer_const_literals_to_create_immutables
+        child: Column(
+          children: [
+            Center(
+              child: const Text(
+                "Welcome to Kutoa!",
                 style: TextStyle(
                   backgroundColor: Colors.white10,
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-                ),),
-          ),
-              //space
-              const SizedBox(height: 20,),
-              //text
-               Center(
-                 child: Text("Your Donations and Receiving App!",
-              style: TextStyle(
+                ),
+              ),
+            ),
+            //space
+            const SizedBox(
+              height: 20,
+            ),
+            //text
+            Center(
+              child: Text(
+                "Your Donations and Receiving App!",
+                style: TextStyle(
                   backgroundColor: Colors.white10,
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-              ),),
-               ),
-               //space
-              const SizedBox(height: 20,),
-              //text
-               Center(
-                 child: Text("Donate your unused item or choose a donated item to receive",
-              style: TextStyle(
+                ),
+              ),
+            ),
+            //space
+            const SizedBox(
+              height: 20,
+            ),
+            //text
+            Center(
+              child: Text(
+                "Donate your unused item or choose a donated item to receive",
+                style: TextStyle(
                   backgroundColor: Colors.white10,
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
-              ),),
-               ),
-               //space
-              const SizedBox(height: 20,),
-              TextButton(
-                      child: Text("Login".toUpperCase(),
-                          style: TextStyle(fontSize: 14)),
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.all(15)),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide(color: Colors.orange)))),
-                      onPressed: () async {
-                Get.to(() => Login());
-                      }),
-                       //space
-              const SizedBox(height: 20,),
-               TextButton(
-                      child: Text("Signup".toUpperCase(),
-                          style: TextStyle(fontSize: 14)),
-                      style: ButtonStyle(
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.all(15)),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide(color: Colors.orange)))),
-                      onPressed: () async {
-                Get.to(() => Signup());
-                      }),
-              ],
-      ), 
-    ),);
+                ),
+              ),
+            ),
+            //space
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                child:
+                    Text("Login".toUpperCase(), style: TextStyle(fontSize: 14)),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Colors.orange)))),
+                onPressed: () async {
+                  Get.to(() => Login());
+                }),
+            //space
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                child: Text("Signup".toUpperCase(),
+                    style: TextStyle(fontSize: 14)),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(15)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: BorderSide(color: Colors.orange)))),
+                onPressed: () async {
+                  Get.to(() => Signup());
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
